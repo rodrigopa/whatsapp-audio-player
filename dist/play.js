e7eb3a3c1e230c449b0f7e33e39c51372e760527
Rodrigo <rodrigopa@Rodrigos-Air.lan>
2023-07-30 13:15:40 -0300

    add dist folder

---
+++ dist/play.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const Play = () => {
+    return (React.createElement("svg", { width: "20px", height: "20px", viewBox: "-0.5 0 7 7", version: "1.1", xmlns: "http://www.w3.org/2000/svg" },
+        React.createElement("g", { id: "Page-1", stroke: "none", strokeWidth: "1", fill: "none", fillRule: "evenodd" },
+            React.createElement("g", { id: "Dribbble-Light-Preview", transform: "translate(-347.000000, -3766.000000)", fill: "#9b8e8d" },
+                React.createElement("g", { id: "icons", transform: "translate(56.000000, 160.000000)" },
+                    React.createElement("path", { d: "M296.494737,3608.57322 L292.500752,3606.14219 C291.83208,3605.73542 291,3606.25002 291,3607.06891 L291,3611.93095 C291,3612.7509 291.83208,3613.26444 292.500752,3612.85767 L296.494737,3610.42771 C297.168421,3610.01774 297.168421,3608.98319 296.494737,3608.57322", id: "play-[#1003]" }))))));
+};
---
+++ dist/player.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+type Props = {
+    audioFile: string;
+    dark?: boolean;
+};
+export declare const WhatsappAudioPlayer: React.FC<Props>;
+export {};
